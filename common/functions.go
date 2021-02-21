090ff5771c2bfc8eb953eda854420d03ff799f21
aleass <g614547908@gmail.com>
2021-02-21 19:51:35 +0800

    test2

--- common/functions.go
+++ common/functions.go
@@ -1,18 +1,18 @@
 package common
 
 //传入顺序切片值和查找数，返回index，不存在返回0
-func BinarySearch(i []int,j int) int{
+func BinarySearch(i []int, j int) int {
 	res := i[len(i)/2]
-	index := len(i)/2
+	index := len(i) / 2
 	if res == j {
 		return index
-	}else if res > j {
-		return BinarySearch(i[:index-1],j) + index
-	}else if res <j {
-		return BinarySearch(i[index:],j) + index
+	} else if res > j {
+		return BinarySearch(i[:index-1], j) + index
+	} else if res < j {
+		return BinarySearch(i[index:], j) + index
 	}
 	return 0
 }
+func test() {
 
-
-
+}
