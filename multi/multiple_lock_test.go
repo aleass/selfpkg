e20785d33ab9da979bb554267389ccd757371916
tuski <g614547908@gmail.com>
2022-06-20 23:49:49 +0800

    feature(download): 添加分段锁

---
+++ multi/multiple_lock_test.go
@@ -0,0 +1,12 @@
+package multi
+
+import "testing"
+
+func TestMulLockDataTypeAll(t *testing.T) {
+	//mulLock := NewMulLockData()
+	//go func() {
+	//	for i := 0; i < 10000; i++ {
+	//		go mulLock.Add(i, i)
+	//	}
+	//}()
+}
