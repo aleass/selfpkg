2033fdbdaa5bb67994275552654dd6eb4d49ffea
guanziliang <gzljszgl@qq.com>
2022-07-21 09:14:35 +0800

    feature: add func

---
+++ functions/funcs.go
@@ -0,0 +1,14 @@
+package functions
+
+import "fmt"
+
+func NumChanges(v float64)string {
+	if v < 1e8 {
+		v = v / 1e4
+		return  fmt.Sprintf("%0.2f%s", v, "万")
+	} else {
+		v = v / 1e8
+		return fmt.Sprintf("%0.2f%s", v, "亿")
+	}
+
+}
